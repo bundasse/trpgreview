a1d8524bdd2f95b520f72d3521fb682f72757eb5
bundasse <ymtotolove@gmail.com>
2023-11-04 15:05:19 +0900

    1104 베이스

---
+++ src/components/CommentComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="basis-1/5 padding-5 rounded-md flex flex-col">
+        <div class="reviewTop">상단배너</div>
+        <div class="avatar w-10 rounded-full"><img src="" alt=""></div>
+        <div class="reviewBottom">
+            <p class="reviewName"></p>
+            <p class="reviewContents"></p>
+        </div>
+        <button @click.prevent>좋아요</button>
+    </div>
+</template>
+<script setup>
+const reviews = ref([]);
+
+</script>
+<style>
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "./index.css"
 
-createApp(App).use(store).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret)
+
+createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
