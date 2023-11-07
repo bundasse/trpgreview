249df204e776e21e7779da643afae8b2983e924d
bundasse <ymtotolove@gmail.com>
2023-11-07 22:18:52 +0900

    1107 gnb 배치 수정

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -1,12 +1,16 @@
 <template>
     <!-- 데스크탑 -->
     <div class="gnb max-w-7xl mx-auto h-20 flex justify-between items-center">
+        <h1><router-link to="/">로고</router-link></h1>
+        <div>
+            <input type="" name="" value="" placeholder="찾고싶은 사람의 닉네임을 입력해보세요">
+            <button></button>
+        </div>
         <ul class="flex gap-20">
-            <li><router-link to="/">로고</router-link></li>
+            <li></li>
             <li>캘린더</li>
             <li>나만의 후기</li>
         </ul>
-        <div>검색바</div>
         <div class="myPage basis-28 text-right">
             <div v-if="loginOn === false" @click="loginOn = !loginOn">로그인</div>
             <div v-else class="flex gap-3 justify-end" @click="loginOn = !loginOn">
