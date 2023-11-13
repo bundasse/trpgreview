fe4c329454d05c9d783f06c3ff3c32a188ad7e86
bundasse <ymtotolove@gmail.com>
2023-11-13 22:46:33 +0900

    1113 로그인

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <form action="">
+            <input type="text">아이디
+            <input type="text">패스워드
+            <button>로그인</button>
+        </form>        
+    </div>
+</template>
+<script setup>
+
+</script>
+<style>
+    
+</style>
